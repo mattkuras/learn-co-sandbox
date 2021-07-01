c6de5130cec08235f9b6467b895ec912c13b5ee4
Matt Kuras <mattkuras@gmail.com>
2021-07-01 22:26:41 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,9 @@
+new_hash = {
+  :created => Time.now,
+  :message => "Hello world!"
+}
+new_hash 
+
+second_new_hash = Hash.new 
+second_new_hash = {}
+
---
+++ learn-comments.rb
@@ -0,0 +1,12 @@
+case name 
+when "alice"
+  puts "hey alice"
+when "white rabbit"
+  puts "dont be fucking late again"
+when "the mad hatter"
+  puts "lez get drunk"
+when "Queen of hearts"
+  puts "yikes"
+else 
+  puts "whoo are u?"
+end
